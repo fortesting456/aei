7152e7c0b61dacf6ef255bf38a61daa644f3a54c
jaivincoder <jaivinmax222@gmail.com>
2024-04-12 17:15:52 -0400

    h

---
+++ assets/js/custom.js
@@ -0,0 +1,57 @@
+/* ==========================================
+scrollTop() >= 300
+Should be equal the the height of the header
+========================================== */
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 200) {
+        $('.main-header').addClass('fixed-header');
+    }
+    else {
+        $('.main-header').removeClass('fixed-header');
+    }
+});
+
+$('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+});
+
+
+var btn = $('#top-button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+/* Our Values Carousel */
+$('.our-values-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    autoplay:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1.1,
+            margin:10,
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1.5
+        }
+    }
+})
